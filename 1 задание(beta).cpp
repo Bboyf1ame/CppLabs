1c284af0fa8e58b033d205da46facf765fb177a1
Nikita <neo277@mail.ru>
2017-09-17 16:13:32 +0500

    1 задание

---
+++ 1 задание(beta).cpp
@@ -0,0 +1,29 @@
+#include <iostream> 
+#include <math.h>
+#include <conio.h>
+using namespace std;
+int main()
+{
+	double x, y, z;
+	cin >> x, y, z;
+	int a, b;
+	cout << "a = ";
+	cin >> a;
+	cout << "b = ";
+	cin >> b;
+	      {
+		   int n;
+	       int i;
+	       int res;
+
+	       cin >> n;
+	       res = 1;
+	       for (i = 1; i <= n; i++);
+	      }
+		  cout << log(fabs((y - sqrt(fabs(x))))(x - ((y / (z + pow(x, 2) / 4)))));
+    cin >> log(fabs((y - sqrt(fabs(x))))(x - ((y/(z + pow(x,2) /4)))));
+    cout << x - pow(x, 2) / 3 <= x +pow(x, 5) / 5 <=x;
+	cin >> x - pow(x, 2) / 3 <= x +pow(x, 5) / 5 <=x;
+	system("pause");
+	return 0;
+}
