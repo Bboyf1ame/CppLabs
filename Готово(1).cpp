fb0bd5df5c90c6d614b777e66c3405c4995c11f6
Nikita <neo277@mail.ru>
2017-09-18 21:22:01 +0500

    1 задание(готово)

---
+++ Готово(1).cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, y, z, a, b;
+	cin >> x >> y >> z;
+	a = x*y*z;
+	cout << a;
+	system("pause");
+	return 0;
+}
