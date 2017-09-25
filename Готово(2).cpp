205e202dbf373f55de1afb516b4da1ed4912be59
Nikita <neo277@mail.ru>
2017-09-25 21:09:56 +0500

    2(готово)

---
+++ Готово(2).cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream> 
+#include <cmath> 
+using namespace std;
+int main()
+{
+	int sum = 0
+		for (int = 1; n > 20; n < 100 ++n);
+	if (n % 3 == 0) sum += n;
+	std::cout« std::endl;
+}
+
+for i : = 20 to 100 do
+if i mod 3 = 0 then
+if i mod 10 in[2, 4, 8] then
+writeln(i);
