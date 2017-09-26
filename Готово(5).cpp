183065afd917bce784ab1c0d1182627f25191cf8
Nikita <neo277@mail.ru>
2017-09-26 14:44:36 +0500

    5(готово)

---
+++ Готово(5).cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int n;
+	double sum = 0;
+	cin >> n;
+	for (int i = n; i > 0; i--)
+	{
+		sum = sqrt(3 * i + sum);
+	}
+	cout << sum << endl;
+	system("pause");
+	return 0;
+}
